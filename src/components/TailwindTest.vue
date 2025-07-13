3bca63a07b401093f441bcd852fd302cfcb879be
gerson <gersonmoliver@gmail.com>
2025-07-12 22:34:44 -0700

    installed tailwindcss

---
+++ src/components/TailwindTest.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-amber-400">
+    <p class="bg-pink-600 text-white p-4 font-extrabold">Teste Tailwind ativo!</p>
+  </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  
 })
