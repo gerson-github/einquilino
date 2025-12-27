ca1445d83dfee8b4223b0fd9e4c1c7cf26668d06
gerson <gersonmoliver@gmail.com>
2025-12-27 01:38:24 -0800

    update row position for main.css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import "./assets/main.css"; // importa os estilos globais da aplicacao
 import { createApp } from "vue"; // Importa a função para criar a instância principal do Vue.
 import App from "./App.vue"; // Importa o componente raiz (App.vue).
 import i18n from "./i18n";
 import router from "../router"; // mporta o roteador (configurações de navegação).
+import "./assets/main.css"; // importa os estilos globais da aplicacao
 
 createApp(App)
   .use(router) // aqui Adiciona o sistema de rotas (Vue Router) à aplicação.
