5a8f48e3d6de4414511c5d9077d8add32e810b3a
gerson <gersonmoliver@gmail.com>
2025-12-29 10:10:32 -0800

    implemented api call for contracts

--- src/views/Contratos.vue
+++ src/views/Contratos.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-6xl mx-auto p-6 space-y-8">
     <h1 class="text-2xl font-bold text-gray-800">
-      {{ template.templateName }}
+      {{ templates.templateName }}
     </h1>
 
     <!-- Progress Bar -->
@@ -119,94 +119,111 @@
 import { ref, onMounted } from "vue";
 import FieldRenderer from "@/components/fieldRenderer.vue";
 
-const template = ref({ groups: [] });
-const formData = ref({});
-const currentStep = ref(0);
+import { useTemplates } from "../composables/useTemplates";
+
+const { templates, loading, error, loadTemplates } = useTemplates();
 
 onMounted(() => {
-  template.value = {
-    templateName: "Residential Lease Agreement",
-    groups: [
-      {
-        id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
-        name: "Template de Teste",
-        fields: [
-          {
-            id: "11111111-1111-1111-1111-111111111111",
-            key: "tenant_name",
-            label: "Tenant Name",
-            type: "text",
-            required: true,
-            position: 1,
-            options: null,
-            settings: {
-              placeholder: "Full legal name",
-            },
-          },
-          {
-            id: "22222222-2222-2222-2222-222222222222",
-            key: "start_date",
-            label: "Start Date",
-            type: "date",
-            required: true,
-            position: 2,
-            options: null,
-            settings: null,
-          },
-          {
-            id: "33333333-3333-3333-3333-333333333333",
-            key: "end_date",
-            label: "End Date",
-            type: "date",
-            required: false,
-            position: 3,
-            options: null,
-            settings: {
-              visible_if: {
-                field: "start_date",
-                not_null: true,
-              },
-            },
-          },
-          {
-            id: "44444444-4444-4444-4444-444444444444",
-            key: "rent_amount",
-            label: "Monthly Rent Amount",
-            type: "number",
-            required: true,
-            position: 4,
-            options: {
-              min: 0,
-              currency: "USD",
-            },
-            settings: {
-              format: "currency",
-            },
-          },
-          {
-            id: "55555555-5555-5555-5555-555555555555",
-            key: "payment_frequency",
-            label: "Payment Frequency",
-            type: "select",
-            required: true,
-            position: 5,
-            options: {
-              options: ["monthly", "bi-weekly"],
-            },
-            settings: null,
-          },
-        ],
-      },
-    ],
-  };
 
-  // initialize formData
-  template.value.groups.forEach((group) => {
-    group.fields.forEach((field) => {
-      formData.value[field.key] = "";
-    });
-  });
+  console.log("step1 contratos VUE");
+
+  loadTemplates();
+
+  console.log("step2 loadTemplate executed !!");
+
 });
+
+
+// const template = ref({ groups: [] });
+// const formData = ref({});
+// const currentStep = ref(0);
+
+// onMounted(() => {
+//   template.value = {
+//     templateName: "Residential Lease Agreement",
+//     groups: [
+//       {
+//         id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+//         name: "Template de Teste",
+//         fields: [
+//           {
+//             id: "11111111-1111-1111-1111-111111111111",
+//             key: "tenant_name",
+//             label: "Tenant Name",
+//             type: "text",
+//             required: true,
+//             position: 1,
+//             options: null,
+//             settings: {
+//               placeholder: "Full legal name",
+//             },
+//           },
+//           {
+//             id: "22222222-2222-2222-2222-222222222222",
+//             key: "start_date",
+//             label: "Start Date",
+//             type: "date",
+//             required: true,
+//             position: 2,
+//             options: null,
+//             settings: null,
+//           },
+//           {
+//             id: "33333333-3333-3333-3333-333333333333",
+//             key: "end_date",
+//             label: "End Date",
+//             type: "date",
+//             required: false,
+//             position: 3,
+//             options: null,
+//             settings: {
+//               visible_if: {
+//                 field: "start_date",
+//                 not_null: true,
+//               },
+//             },
+//           },
+//           {
+//             id: "44444444-4444-4444-4444-444444444444",
+//             key: "rent_amount",
+//             label: "Monthly Rent Amount",
+//             type: "number",
+//             required: true,
+//             position: 4,
+//             options: {
+//               min: 0,
+//               currency: "USD",
+//             },
+//             settings: {
+//               format: "currency",
+//             },
+//           },
+//           {
+//             id: "55555555-5555-5555-5555-555555555555",
+//             key: "payment_frequency",
+//             label: "Payment Frequency",
+//             type: "select",
+//             required: true,
+//             position: 5,
+//             options: {
+//               options: ["monthly", "bi-weekly"],
+//             },
+//             settings: null,
+//           },
+//         ],
+//       },
+//     ],
+//   };
+
+//   // initialize formData
+//   template.value.groups.forEach((group) => {
+//     group.fields.forEach((field) => {
+//       formData.value[field.key] = "";
+//     });
+//   });
+
+
+// });
 
 const nextStep = () => {
   if (currentStep.value < template.value.groups.length - 1) {
