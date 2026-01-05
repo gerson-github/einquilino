68f87101e323368fede7f4217647870946d80b69
gerson <gersonmoliver@gmail.com>
2026-01-05 01:51:43 -0800

    add /api prefix

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,13 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/indicators": {
+      "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
+        secure: false,  
       },
     },
-  },
+  },  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
