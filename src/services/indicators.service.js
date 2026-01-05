070ce0a51e5f33bc816a8a2e80d572b79971e5ac
gerson <gersonmoliver@gmail.com>
2026-01-05 03:08:36 -0800

    bug fixed when passed data json

--- src/services/indicators.service.js
+++ src/services/indicators.service.js
@@ -1,14 +1,7 @@
-// services/contracts.service.js
 import request from "./api";
 
-// export function getMonthlyIndicators() {
-//   return request("/indicators/monthly");
-// }
-
 export async function getMonthlyIndicators() {
-  const response = await request('/indicators/monthly');
-  if (!response.ok) throw new Error('Failed to fetch indicators');
-  const data = await response.json();
+  const data = await request('/indicators/monthly');
   return data.indicators; // apenas os valores internos
 }
 
