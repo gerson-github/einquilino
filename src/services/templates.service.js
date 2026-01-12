bbb452bf5833bd22997bd42b06d2dbc7819287b2
gerson <gersonmoliver@gmail.com>
2026-01-12 02:06:11 -0800

    refactored code

--- src/services/templates.service.js
+++ src/services/templates.service.js
@@ -1,15 +1,6 @@
 import request from "./api";
 
-export async function getTemplates(id) {
-  console.log("step1 getTemplates");
-
-  const response = await fetch(`/templates/${id}`);
-
-  console.log("step2 getTemplates");
-  console.log(response);
-
-  if (!response.ok) throw new Error("Failed to fetch templates");
-  const data = await response.json();
-
-  return data; // apenas os valores internos
+export async function getTemplate(templateId) {
+  // example endpoint: /templates/:id
+  return await request(`/templates/${templateId}`);
 }
