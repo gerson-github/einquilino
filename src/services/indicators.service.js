27c00e54544775ae34197dc2344dcf9b769ddd9f
gerson <gersonmoliver@gmail.com>
2026-01-12 02:00:28 -0800

    update the data return

--- src/services/indicators.service.js
+++ src/services/indicators.service.js
@@ -2,6 +2,6 @@ import request from "./api";
 
 export async function getMonthlyIndicators() {
   const data = await request('/indicators/monthly');
-  return data.indicators; // apenas os valores internos
+  return data; // apenas os valores internos
 }
 
